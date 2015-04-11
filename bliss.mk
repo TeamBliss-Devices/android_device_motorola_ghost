9c9c1370d032e9bd0734d55a53508bd5ed7f1ef7
DC07 <deveshnegi.3105.dn@gmail.com>
2015-04-11 04:00:51 -0400

    update

--- bliss.mk
+++ bliss.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_NAME := bliss_ghost
